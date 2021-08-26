12a19c000bff00fe714dbc650e7c2756cd9446ae
Kim Yeseul <yesl00k624@gmail.com>
2021-08-26 23:57:09 +0900

    init

---
+++ react/webpack-test/src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Button(props) {
+  return (
+    React.createElement('button', null, props.label)
+  )
+}
---
+++ vanillaJS-MVC/js/app.js
@@ -0,0 +1,10 @@
+(function () {
+  "use strict";
+  function Todo(name) {
+    this.storage = new app.Store(name);
+    this.model = new app.Model(this.storage);
+    this.template = new app.Template();
+    this.view = new app.View(this.template);
+    this.controller = new app.Controller(this.model, this.view);
+  }
+})();
